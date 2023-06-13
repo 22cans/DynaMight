97f58a6917ca0944999bee5e00b79b5458ee1805
Cussa de Souza <pcussa@22cans.com>
2023-06-13 14:42:44 +0100

    feat(LBE-244): pagination inline doc

--- src/DynaMight/Pagination/IConvertTo.cs
+++ src/DynaMight/Pagination/IConvertTo.cs
@@ -1,6 +1,12 @@
 namespace DynaMight.Pagination;
 
 #pragma warning disable CA2252
+/// <summary>
+/// Interface to specify the conversion method from the type TX to type TY, both generics.
+/// You will need to implement the method ConvertFrom.
+/// </summary>
+/// <typeparam name="TX"></typeparam>
+/// <typeparam name="TY"></typeparam>
 public interface IConvertFrom<in TX, out TY>
 {
     static abstract TY ConvertFrom(TX original);
--- src/DynaMight/Pagination/Page.cs
+++ src/DynaMight/Pagination/Page.cs
@@ -1,5 +1,11 @@
 namespace DynaMight.Pagination;
 
+/// <summary>
+/// Paged list of Results of Type `T`.
+/// </summary>
+/// <param name="PageToken">The page token used by DynamoDB to do pagination</param>
+/// <param name="Results">The list of results for the query</param>
+/// <typeparam name="T">The mapped type from the DynamoDB to the C# class</typeparam>
 public sealed record Page<T>(string PageToken, IList<T>? Results)
 {
     public Page<TY> ConvertTo<TY>() where TY : IConvertFrom<T, TY>
