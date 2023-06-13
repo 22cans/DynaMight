c5de54f0c4616d84a10167e457c5376f45a997e6
Cussa de Souza <pcussa@22cans.com>
2023-06-13 10:19:16 +0100

    feat(LBE-244): atomic operations inline doc

--- src/DynaMight/AtomicOperations/AddAtomicOperation.cs
+++ src/DynaMight/AtomicOperations/AddAtomicOperation.cs
@@ -7,6 +7,18 @@ public class AddAtomicOperation<T> : AtomicOperation<T>
     public override string UpdateExpressionType => "ADD";
     protected override string Pattern => "#{0} :{0}";
 
+    /// <summary>
+    /// Creates an atomic operation that will use the ADD operation in the DynamoDB.
+    /// </summary>
+    /// <remarks>
+    /// Use the ADD action in an update expression to add a new attribute and its values to an item.
+    /// If the attribute already exists, the behavior of ADD depends on the attribute's data type:
+    /// - If the attribute is a number, and the value you are adding is also a number, the value is mathematically added to the existing attribute. (If the value is a negative number, it is subtracted from the existing attribute.)
+    /// - If the attribute is a set, and the value you are adding is also a set, the value is appended to the existing set.
+    /// For more information, check https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Expressions.UpdateExpressions.html#Expressions.UpdateExpressions.ADD
+    /// </remarks>
+    /// <param name="fieldName">The field's name that will be added. Recommended to use `nameof` function to avoid issues when renaming.</param>
+    /// <param name="value">The value that will be added.</param>
     public AddAtomicOperation(string fieldName, T value)
     {
         FieldName = fieldName;
--- src/DynaMight/AtomicOperations/IncrementByAtomicOperation.cs
+++ src/DynaMight/AtomicOperations/IncrementByAtomicOperation.cs
@@ -8,6 +8,14 @@ public class IncrementByAtomicOperation<T> : AtomicOperation<T>
 {
     protected override string Pattern => "#{0} = #{0} + :{0}Increment";
 
+    /// <summary>
+    /// Creates an atomic operation that will use the SET operation in the DynamoDB with an addition operation (`field = field + value`).
+    /// </summary>
+    /// <remarks>
+    /// If the field does not exist in the register, it will throw an exception. 
+    /// </remarks>
+    /// <param name="fieldName">The field's name that will be added. Recommended to use `nameof` function to avoid issues when renaming.</param>
+    /// <param name="increment">The value that will be incremented.</param>
     public IncrementByAtomicOperation(string fieldName, T increment)
     {
         FieldName = fieldName;
