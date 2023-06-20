5d807784947df73b2baf29d1e1de3273fb8e5175
Cussa de Souza <pcussa@22cans.com>
2023-06-20 12:26:39 +0100

    docs(LBE-244): extensions inline docs

--- src/DynaMight/Extensions/DictionaryExtensions.cs
+++ src/DynaMight/Extensions/DictionaryExtensions.cs
@@ -1,7 +1,17 @@
 namespace DynaMight.Extensions;
 
+/// <summary>
+/// Dictionary Extensions Methods
+/// </summary>
 public static class DictionaryExtensions
 {
+    /// <summary>
+    /// Adds a tuple Key-ValuePair into the dictionary
+    /// </summary>
+    /// <param name="dict">The dictionary that will receive the new values</param>
+    /// <param name="tuple">Tuple containing a key and a value</param>
+    /// <typeparam name="TK">The Key's type</typeparam>
+    /// <typeparam name="TV">The Value's type</typeparam>
     public static void Add<TK, TV>(this Dictionary<TK, TV> dict, (TK key, TV value) tuple) where TK : notnull
         => dict.Add(tuple.key, tuple.value);
 }
