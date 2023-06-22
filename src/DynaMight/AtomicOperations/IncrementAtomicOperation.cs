d94f1f9b0cecc31c4c7947dc79455298e353dbdd
Cussa de Souza <pcussa@22cans.com>
2023-06-22 14:40:50 +0100

    feat(LBE-422): use `ADD` in Increment operation

--- src/DynaMight/AtomicOperations/IncrementAtomicOperation.cs
+++ src/DynaMight/AtomicOperations/IncrementAtomicOperation.cs
@@ -1,7 +1,7 @@
 namespace DynaMight.AtomicOperations;
 
 /// <summary>
-/// Creates an atomic operation that will use the SET operation in the DynamoDB with an addition operation by value 1 (`field = field + 1`).
+/// Creates an atomic operation that will use the ADD operation in the DynamoDB with value 1 (`ADD #field 1`).
 /// </summary>
 public class IncrementAtomicOperation : IncrementByAtomicOperation<int>
 {
@@ -9,9 +9,6 @@ public class IncrementAtomicOperation : IncrementByAtomicOperation<int>
     /// <summary>
     /// Creates an atomic operation that will increment the current value by 1.
     /// </summary>
-    /// <remarks>
-    /// If the field does not exist in the register, it will throw an exception. 
-    /// </remarks>
     /// <param name="fieldName">The field's name that will be added. Recommended to use `nameof` function to avoid issues when renaming.</param>
     public IncrementAtomicOperation(string fieldName) : base(fieldName, 1) { }
 }
--- src/DynaMight/AtomicOperations/IncrementByAtomicOperation.cs
+++ src/DynaMight/AtomicOperations/IncrementByAtomicOperation.cs
@@ -4,30 +4,11 @@ using DynaMight.Converters;
 
 namespace DynaMight.AtomicOperations;
 
-/// <summary>
-/// Creates an atomic operation that will use the SET operation in the DynamoDB with an addition operation (`field = field + value`).
-/// </summary>
-public class IncrementByAtomicOperation<T> : AtomicOperation<T>
+/// <inheritdoc />
+public class IncrementByAtomicOperation<T> : AddAtomicOperation<T>
 {
     /// <inheritdoc />
-    protected override string Pattern => "#{0} = #{0} + :{0}Increment";
-
-    /// <summary>
-    /// Creates an atomic operation that will use the SET operation in the DynamoDB with an addition operation (`field = field + value`).
-    /// </summary>
-    /// <remarks>
-    /// If the field does not exist in the register, it will throw an exception. 
-    /// </remarks>
-    /// <param name="fieldName">The field's name that will be added. Recommended to use `nameof` function to avoid issues when renaming.</param>
-    /// <param name="increment">The value that will be incremented.</param>
-    public IncrementByAtomicOperation(string fieldName, T increment)
+    public IncrementByAtomicOperation(string fieldName, T increment) : base(fieldName, increment)
     {
-        FieldName = fieldName;
-        AttributeValue = new AttributeValue { N = increment!.ToString() };
-        Value = increment;
     }
-
-    /// <inheritdoc />
-    public override (string, AttributeValue, DynamoDBEntry?) GetValueExpression()
-        => ($":{FieldName}Increment", AttributeValue, DynamoValueConverter.ToDynamoDbEntry(Value));
 }
